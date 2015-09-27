fc5e268bf012eba25af39f14dfe62a6caa1e7884
David Scott <dave@recoil.org>
2015-09-27 22:27:33 +0100

    Attempt to measure coverage from travis, with coveralls
    Note this bumps the travis OCaml version to 4.02 to be compatible
    with ocveralls.
    
    Signed-off-by: David Scott <dave@recoil.org>

---
+++ .coveralls.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+eval `opam config env`
+opam install ocveralls -y
+make
+make test
+BISECT_FILE=_build/coverage ./setup.bin -test
+`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
