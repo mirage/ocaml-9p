260334cbf0924751425c12a4b5f5232c8616aa5c
David Scott <dave@recoil.org>
2015-09-27 23:43:16 +0100

    coveralls: add --enable-tests
    Signed-off-by: David Scott <dave@recoil.org>

--- .coveralls.sh
+++ .coveralls.sh
@@ -2,7 +2,6 @@
 
 eval `opam config env`
 opam install ocveralls -y
-make
-make test
+make TESTS=--enable-tests
 BISECT_FILE=_build/coverage ./setup.bin -test
 `opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
