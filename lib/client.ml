8dd3d172639730321be47ed29a8dcc82005c003e
David Scott <dave.scott@unikernel.com>
2015-09-28 15:33:05 +0100

    Add a (completely untested) Client module
    So far the only thing implemented is version negotiation.
    
    Signed-off-by: David Scott <dave.scott@unikernel.com>

---
+++ lib/client.ml
@@ -0,0 +1,97 @@
+(*
+ * Copyright (C) 2015 David Scott <dave.scott@unikernel.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+open Result
+open Error
+
+module Make(FLOW: V1_LWT.FLOW) = struct
+
+  type t = {
+    msize: int32;
+    flow: FLOW.flow;
+    mutable input_buffer: Cstruct.t;
+  }
+
+  (* For converting flow errors *)
+  let (>>|=) m f =
+    let open Lwt in
+    m >>= function
+    | `Ok x -> f x
+    | `Eof -> return (error_msg "Caught EOF on underlying FLOW")
+    | `Error e -> return (error_msg "Unexpected error on underlying FLOW: %s" (FLOW.error_message e))
+
+  (* For Result + Lwt *)
+  let (>>*=) m f =
+   let open Lwt in
+   m >>= function
+   | Ok x -> f x
+   | Error x -> Lwt.return (Error x)
+
+  let read_exactly t n =
+    let output = Cstruct.create n in
+    let rec fill tofill = match Cstruct.len tofill with
+      | 0 -> Lwt.return (Ok ())
+      | n ->
+        ( if Cstruct.len t.input_buffer = 0
+          then (FLOW.read t.flow >>|= fun b -> Lwt.return (Ok b))
+          else Lwt.return (Ok t.input_buffer)
+        ) >>*= fun input ->
+        let avail = min n (Cstruct.len input) in
+        Cstruct.blit input 0 tofill 0 avail;
+        t.input_buffer <- Cstruct.shift input avail;
+        fill (Cstruct.shift tofill avail) in
+    fill output
+    >>*= fun () ->
+    Lwt.return (Ok output)
+
+  let read_one_packet t =
+    let length_buffer = Cstruct.create 4 in
+    read_exactly t 4
+    >>*= fun length_buffer ->
+    let length = Cstruct.LE.get_uint32 length_buffer 0 in
+    read_exactly t (Int32.to_int length)
+    >>*= fun packet_buffer ->
+    (* XXX: remove this data copy *)
+    let buffer = Cstruct.create (Cstruct.len length_buffer + (Cstruct.len packet_buffer)) in
+    Cstruct.blit length_buffer 0 buffer 0 (Cstruct.len length_buffer);
+    Cstruct.blit packet_buffer 0 buffer (Cstruct.len length_buffer) (Cstruct.len packet_buffer);
+    Lwt.return (Ok buffer)
+
+  let connect flow ~msize =
+    let client_version = {
+      Request.tag = Types.Tag.notag;
+      payload = Request.Version Request.Version.({ msize; version = Types.Version.default });
+    } in
+    let sizeof = Request.sizeof client_version in
+    let buffer = Cstruct.create sizeof in
+    Lwt.return (Request.write client_version buffer)
+    >>*= fun _ ->
+    FLOW.write flow buffer
+    >>|= fun () ->
+
+    let t = { flow; msize; input_buffer = Cstruct.create 0 } in
+
+    read_one_packet t
+    >>*= fun buffer ->
+    Lwt.return (Response.read buffer)
+    >>*= fun (response, _) ->
+    match response with
+    | { Response.payload = Response.Version { Response.Version.msize; version }} when version = Types.Version.default ->
+      let msize = min t.msize msize in
+      Lwt.return (Ok { t with msize })
+    | _ ->
+      Lwt.return (error_msg "Server sent unexpected version reply: %s" (Response.to_string response))
+end
