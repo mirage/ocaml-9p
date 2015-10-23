b1157c753e809cedfac3a6ecd47fc928d3f0d640
David Scott <dave.scott@unikernel.com>
2015-10-23 19:40:02 +0100

    travis: attempt to use @simonjbeaumont's coveralls
    - Remove bisect from _oasis (Fixes #6)
    - Run travis-coveralls.sh
    
    Signed-off-by: David Scott <dave.scott@unikernel.com>

--- .coveralls.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-eval `opam config env`
-opam install ocveralls -y
-make TESTS=--enable-tests
-BISECT_FILE=_build/coverage ./setup.bin -test
-`opam config var bin`/ocveralls --prefix _build _build/coverage*.out --send
